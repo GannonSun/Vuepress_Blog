b757df96736a272b1b3699a094092a8ede6ddb78
GannonSun <2621806555@qq.com>
2021-01-10 00:15:08 +0800

    提交代码

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,25 @@
 module.exports = {
     base: "/Vuepress_Blog/",
+    head: [
+        ["link", {
+            rel: "icon",
+            href: "/GannonSun.png"
+        }]
+    ],
     title: "Hello",
-    description: "My Bolg"
+    description: "My Bolg",
+    themeConfig: {
+        repo: "https://github.com/gannonsun/Vuepress_Blog",
+        repoLable: "Github",
+        nav: [
+            {
+                text: "首页",
+                link: "/"
+            },
+            {
+                text: "百度",
+                link: "https://baidu.com"
+            }
+        ]
+    }
 }
